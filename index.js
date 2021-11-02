305f6eb886f762f6e64e1b4995db50abe5aeba80
igson <igson660@gmail.com>
2021-11-02 11:46:17 -0500

    instala o cors

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const bodyParser = require('body-parser');
 const express = require('express');
+const cors = require('cors');
 
 const app = express();
 app.use(bodyParser.json());
+app.use(cors())
 
 const { PORT= 3001 } = process.env;
 const Task = require('./controllers/taskController');
